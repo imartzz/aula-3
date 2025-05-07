33a4cc7f5ecadc9b16db83207f7294e4b7713d14
Arthur De Pinho De Almeida <arthur.almeida.1593190@sga.pucminas.br>
2025-05-07 11:37:09 +0000

    Versao final

--- exemplo1.js
+++ exemplo1.js
@@ -1,19 +1,23 @@
-function lerEntrada(){
-    document.getElementById("resp").innerHTML = ""
+function lerEntrada() {
+    //Limpar o painel de resposta:
+    document.getElementById("resp").innerHTML = "";
 
+    //Ler a idade:
     var idade = document.getElementById("idade").value;
-    if(idade != "" && idade != undefined && idade != null) {
-    document.getElementById("resp").innerHTML = `Idade Informada: ${idade}`;
-    votacao(idade);
+
+    //Apresentar a informação no painel de resposta:
+    if (idade != "" && idade != undefined && idade != null) {
+        document.getElementById("resp").innerHTML = `<div class='tile'>Idade informada: ${idade} </div>`;
+        votacao(idade);
     }
 }
-function votacao(idade){
-    if(idade < 18) {
-     document.getElementById("resp").innerHTML += "div class=`tile` >A pessoa é menor de idade.</div>";
-     document.getElementById("resp").innerHTML += "div class=`tile` >A pessoa não pode votar.</div>";
+
+function votacao(idade) {
+    if (idade < 18) {
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é menor de idade.</div>";
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa não pode votar.</div>";
+    } else {
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa é maior de idade.</div>";
+        document.getElementById("resp").innerHTML += "<div class='tile'>A pessoa pode votar.</div>";
     }
-     else{
-    document.getElementById("resp").innerHTML += "div class=`tile` > A pessoa é maior de idade.<div>";
-    document.getElementById("resp").innerHTML += "div class=`tile` > A pessoa pode votar.<div>";
-     }
 }
